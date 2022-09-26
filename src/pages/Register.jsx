21df98616901cfb253f2321d967b9983c59388b3
lyakhnovich <lyakhnovich.german@gmail.com>
2022-09-26 20:03:39 +0300

    initial commit

---
+++ src/pages/Register.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Add from '../img/addAvatar.png'
+
+const Register = () => {
+  return (
+    <div className='formContainer'>
+      <div className='formWrapper'>
+        <span className='logo'>LOL Chat</span>
+        <span className='title'>Register</span>
+        <form>
+          <input type='text' placeholder='display name'/>
+          <input type='email' placeholder='email'/>
+          <input type='password' placeholder='password'/>
+          <input style={{display: "none"}} type='file' id='file'/>
+          <label htmlFor='file'>
+            <img src={Add} alt=''/>
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign Up</button>
+        </form>
+        <p>You do have an account? Log In</p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
