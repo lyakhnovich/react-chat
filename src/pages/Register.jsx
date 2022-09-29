e8fd923ab63943c2a143a6813bc470d925530287
lyakhnovich <lyakhnovich.german@gmail.com>
2022-09-29 23:49:25 +0300

    Firebase

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -22,13 +22,9 @@ const Register = () => {
       const res = await createUserWithEmailAndPassword(auth, email, password);
 
       const storageRef = ref(storage, displayName);
-      const uploadTask = uploadBytesResumable(storageRef, file);
-      uploadTask.on(
-        (error) => {
-          setErr(true)
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+
+      await uploadBytesResumable(storageRef, file).then(() => {
+          getDownloadURL(storageRef).then(async(downloadURL) => {
             await updateProfile(res.user, {
               displayName,
               photoURL: downloadURL
