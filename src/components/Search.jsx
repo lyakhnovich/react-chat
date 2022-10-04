2b881652ec91cb63bb80aea06420f832bf6ccb23
lyakhnovich <lyakhnovich.german@gmail.com>
2022-10-04 19:20:06 +0300

    Firebase

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,17 +1,77 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
+import {collection, query, where, getDocs, setDoc, updateDoc, serverTimestamp} from "firebase/firestore";
+import {db} from "../firebase";
+import {AuthContext} from "../context/AuthContext";
 
 const Search = () => {
+  const [username, setUsername] = useState('');
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+  const {currentUser} = useContext(AuthContext);
+
+  const handleSearch = async () => {
+    const q = query(collection(db, 'users'), where('displayName', '==', username));
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data())
+      });
+    }catch (err) {
+      setErr(true);
+    }
+  }
+
+  const handleKey = e => {
+    e.code === 'Enter' && handleSearch();
+  }
+
+  const handleSelect = async () => {
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser + user.uid
+        : user.uid + currentUser.uid
+
+    try {
+      const res = await getDocs(db, 'chats', combinedId);
+      if (!res.exists()){
+        await setDoc(doc(db, 'chats', combinedId), {messages: []});
+
+        await updateDoc(doc(db, 'userChats', currentUser.uid), {
+          [combinedId + '.userInfo']: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL
+          },
+          [combinedId + '.date']: serverTimestamp()
+        })
+      }
+    }catch (err) {
+      setErr(true)
+    }
+
+  }
+
   return (
     <div className='search'>
       <div className='searchForm'>
-        <input type='text' placeholder='find a user'/>
+        <input
+          type='text'
+          placeholder='find a user'
+          onChange={(e) => setUsername(e.target.value)}
+          onKeyDown={handleKey}
+        />
       </div>
-      <div className='userChat'>
-        <img src='https://avatars.githubusercontent.com/u/60649315?v=4' alt=''/>
+      {err && <span>User not found!</span>}
+      {user && <div className='userChat' onClick={handleSelect}>
+        <img
+          src={user.photoURL}
+          alt=''
+        />
         <div className='userChatInfo'>
-          <span>Kek</span>
+          <span>{user.displayName}</span>
         </div>
-      </div>
+      </div>}
     </div>
   );
 };
