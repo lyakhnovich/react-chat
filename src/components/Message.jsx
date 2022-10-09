c6b740e695feb4149f87d8f94b252cd9b6aac1af
lyakhnovich <lyakhnovich.german@gmail.com>
2022-10-09 10:32:14 +0300

    messages

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {ChatContext} from "../context/ChatContext";
+import {AuthContext} from "../context/AuthContext";
 
-const Message = () => {
+const Message = ({ message }) => {
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
   return (
     <div className='message owner'>
-      <div className='messageInfo'>
-        <img src='https://avatars.githubusercontent.com/u/60649315?v=4' alt=''/>
-        <span>just now</span>
-      </div>
-      <div className='messageContent'>
-        <p>hello</p>
-        <img src='https://avatars.githubusercontent.com/u/60649315?v=4' alt=''/>
-      </div>
+      {/*<div className='messageInfo'>*/}
+      {/*  <img src='https://avatars.githubusercontent.com/u/60649315?v=4' alt=''/>*/}
+      {/*  <span>just now</span>*/}
+      {/*</div>*/}
+      {/*<div className='messageContent'>*/}
+      {/*  <p>hello</p>*/}
+      {/*  <img src='https://avatars.githubusercontent.com/u/60649315?v=4' alt=''/>*/}
+      {/*</div>*/}
     </div>
   );
 };
